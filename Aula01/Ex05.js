fe57ed0b8471ce1acfd58118b1cbbbaeb0fb9ca9
MarcosALPE <marcosyuri2006@gmail.com>
2026-06-02 12:28:55 -0300

    Update Ex05.js

--- Aula01/Ex05.js
+++ Aula01/Ex05.js
@@ -1,4 +1,5 @@
-/*Descrição: Use o confirm() para perguntar ao usuário se ele deseja excluir sua conta. Se confirmar, use alert() para mostrar: "Conta excluída com sucesso". Caso contrário, mostre: "Ação cancelada pelo usuário".*/
+/*Descrição: Use o confirm() para perguntar ao usuário se ele deseja excluir sua conta. Se confirmar, use alert() para mostrar:
+"Conta excluída com sucesso". Caso contrário, mostre: "Ação cancelada pelo usuário".*/
 
 let resposta = confirm("Deseja excluir conta?");
 
