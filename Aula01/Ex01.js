f832daa9727371e69313900a332905ecb72059db
MarcosALPE <marcosyuri2006@gmail.com>
2026-06-02 11:36:37 -0300

    Update Ex01.js

--- Aula01/Ex01.js
+++ Aula01/Ex01.js
@@ -1,3 +1,5 @@
+// Descrição: Crie um programa que solicite ao usuário os seguintes dados: nome, idade , altura, gênero e estudante.
+
 let nome = prompt("Qual é o seu nome?  ") 
 let idade = prompt("Qual é a sua idade? ")
 let altura = prompt("Qual é a sua altura? ")
