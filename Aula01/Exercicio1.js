7a3655da19dceeadf95767fed12a819997513fa6
MarcosALPE <marcosyuri2006@gmail.com>
2026-06-02 10:58:18 -0300

    Adicionar Exercicio1.js na pasta Aula01

---
+++ Aula01/Exercicio1.js
@@ -0,0 +1,17 @@
+let nome = prompt("Qual é o seu nome?  ") 
+let idade = prompt("Qual é a sua idade? ")
+let altura = prompt("Qual é a sua altura? ")
+let genero = prompt("Qual é o seu gênero? ")
+let aluno = prompt("É estudante? ")
+
+alert(`Bem-vindo(a), ${nome}!
+    Seu perfil
+    Nome: ${nome}
+    Idade: ${idade}
+    Altura: ${altura}
+    Gênero: ${genero}
+    Estudante: ${aluno}
+`
+
+
+)
